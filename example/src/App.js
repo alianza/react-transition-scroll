1e321c062b47e9d5c8638b4e38f8151572395bf5
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-09-17 23:14:09 +0200

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
+import logo from "./logo.svg";
 
-import { ExampleComponent } from 'react-transition-scroll'
-import 'react-transition-scroll/dist/index.css'
+import { TransitionScroll } from "react-transition-scroll";
+import 'react-transition-scroll/dist/index.css';
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  return (
+    <div className="App">
+      <header className="bg-blue-500 text-white font-bold flex p-2 justify-between items-center">
+        <div className="grow basis-0">
+          <img src={logo} className="w-12" alt="logo" />
+        </div>
+        <div className="grow basis-0">
+          <p className="text-center text-xl">React Scroll Animation Example</p>
+        </div>
+        <div className="grow basis-0 flex justify-end">
+          <a className="text-end hover:underline" href="https://npmjs.com/package/react-transition-scroll"
+             target="_blank"
+             rel="noopener noreferrer">
+            react-transition-scroll
+          </a>
+        </div>
+      </header>
+      <main className="min-h-screen bg-blue-200">
+        <TransitionScroll reAnimate>
+          <h1>Title</h1>
+        </TransitionScroll>
+      </main>
+    </div>);
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './styles.module.css'
+import * as PropTypes from 'prop-types'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const TransitionScroll = ({
+  threshold = 0,
+  reAnimate = false,
+  children,
+  callBack = () => {},
+  className = ''
+}) => {
+  const articleRef = React.createRef()
+
+  useEffect(() => {
+    const options = { root: null, rootMargin: '0px', threshold }
+
+    const observer = new IntersectionObserver(
+      (entries, observer) =>
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(styles.show)
+            if (!reAnimate) {
+              observer.unobserve(entry.target)
+            }
+          } else {
+            entry.target.classList.remove(styles.show)
+          }
+          callBack(entry)
+        }),
+      options
+    )
+
+    observer.observe(articleRef.current)
+    return () => observer.disconnect()
+  }, [articleRef])
+
+  return (
+    <div ref={articleRef} className={`${styles.hidden} ${className}`}>
+      {children}
+    </div>
+  )
 }
+
+TransitionScroll.propTypes = {
+  threshold: PropTypes.number,
+  reAnimate: PropTypes.bool,
+  children: PropTypes.node.isRequired
+}
+
+export default TransitionScroll
