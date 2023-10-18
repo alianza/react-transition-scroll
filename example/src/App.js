d453ccdd43093fb3cc83017afd8779b8b16da323
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-10-18 22:38:42 +0200

    Dynamic syntax highlighting in example

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
-import cat from "./cat.jpg";
 import styles from "./app.module.scss";
+import utilStyles from "./utils.module.scss";
+
+import hljs from "highlight.js/lib/core";
+import html from "highlight.js/lib/languages/xml";
 
 import GenericTable from "react-generic-table";
 import "react-generic-table/dist/index.css";
-import "react-generic-table/dist/main.css";
 
-const packageUrl = 'https://npmjs.com/package/react-generic-table';
+const packageUrl = "https://npmjs.com/package/react-generic-table";
 
 const App = () => {
+
+  useEffect(() => {
+    hljs.registerLanguage("html", html);
+    hljs.highlightAll();
+    }, []);
+
   return (
     <div className="App">
       <header className={styles.header}>
@@ -18,13 +26,16 @@ const App = () => {
         </div>
 
         <div>
-          <p className={styles.title}>React Scroll Animation Example</p>
+          <p className={styles.title}>React Generic Table Example</p>
         </div>
 
         <div className={styles.linkContainer}>
-          <a className={styles.link} href={packageUrl}
-             target="_blank"
-             rel="noopener noreferrer">
+          <a
+            className={`${utilStyles.link} text-neutral-100`}
+            href={packageUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             react-generic-table on NPM
           </a>
         </div>
@@ -32,16 +43,53 @@ const App = () => {
 
       <main className={styles.main}>
         <div className={styles.container}>
-          <GenericTable
-            objArray={[{id: 1, name: 'J.W.', age: '25', profession: 'Developer'}, {id: 2, name: 'Lea', age: '22', profession: 'Journalist'}]}
-            columns={['name', 'age', 'profession']}
-          />
+          <p>
+            Simple feed data and indicate what columns you want to display!
+            {/*  Nice explanation with emojis */}
+            <span role="img" aria-label="smile">
+              😄
+            </span>
+          </p>
+          <ul>
+            <li>⚙️ Configurable</li>
+            <li>🔀 Out-of-the-box sorting</li>
+            <li>🔄 Loading states</li>
+            <li></li>
+          </ul>
+          <pre>
+            <code>
+              {`
+              <GenericTable
+                objArray={[
+                  { id: 1, name: "J.W.", age: "25", profession: "Developer" },
+                  { id: 2, name: "Lea", age: "22", profession: "Journalist" },
+                ]}
+                columns={["name", "age", "profession"]}
+              />
+              `}
+            </code>
+          </pre>
+          <div className={styles.container}>
+            <GenericTable
+              objArray={[
+                { id: 1, name: "J.W.", age: "25", profession: "Developer" },
+                { id: 2, name: "Lea", age: "22", profession: "Journalist" },
+              ]}
+              columns={["name", "age", "profession"]}
+            />
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
-        <span>Created by <a href="https://jwvbremen.nl/" target="_blank" rel="noreferrer">Jan-Willem van Bremen</a></span>
+        <span>
+          Created by{" "}
+          <a href="https://jwvbremen.nl/" target="_blank" rel="noreferrer">
+            Jan-Willem van Bremen
+          </a>
+        </span>
       </footer>
-    </div>);
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { capitalize, isString, sOrNoS, omit, deepGet } from "./lib/commonUtils";
-import { ChevronDownIcon, ChevronUpDownIcon, ChevronUpIcon, PlusIcon } from '@heroicons/react/20/solid';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
-import { cloneElement } from 'react';
+import { ChevronDownIcon, ChevronUpDownIcon, ChevronUpIcon, PlusIcon } from "@heroicons/react/20/solid";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { cloneElement } from "react";
 
 const duration = 250; // default auto-animate duration
 
@@ -20,31 +20,31 @@ const duration = 250; // default auto-animate duration
  * @returns {JSX.Element} - Generic table component
  * @constructor - GenericTable
  */
-export default function GenericTable({ objArray = null, columns, actions, entityName = 'item', onAction = () => {}, ...options }) {
+function GenericTable({ objArray = null, columns, actions, entityName = "item", onAction = () => {}, ...options }) {
   const [columnSortDirection, setColumnSortDirection] = useState({});
   const [loading, setLoading] = useState(objArray === null);
   const [objArrayState, setObjArrayState] = useState(objArray || []);
   const [tableBody, enableAnimations] = useAutoAnimate();
 
-  if (actions?.length) columns = [...columns, 'actions'];
+  if (actions?.length) columns = [...columns, "actions"];
 
   useEffect(() => {
-    sort(columns[0], 'asc'); // Default ascending sort on first column
+    sort(columns[0], "asc"); // Default ascending sort on first column
   }, []);
 
   useEffect(() => {
     animate(() => {
       setObjArrayState(objArray || []);
       setLoading(objArray === null);
-      if (objArray) sort(columns[0], 'asc'); // Default initial ascending sort on first column
+      if (objArray) sort(columns[0], "asc"); // Default initial ascending sort on first column
     });
   }, [objArray]);
 
   const sort = (column, direction) => {
     setObjArrayState((prevObjArrayState) =>
       prevObjArrayState.sort((a, b) => {
-        if (a[column] > b[column]) return direction === 'asc' ? 1 : -1;
-        if (a[column] < b[column]) return direction === 'asc' ? -1 : 1;
+        if (a[column] > b[column]) return direction === "asc" ? 1 : -1;
+        if (a[column] < b[column]) return direction === "asc" ? -1 : 1;
         return 0;
       }),
     );
@@ -63,89 +63,89 @@ export default function GenericTable({ objArray = null, columns, actions, entity
     <div className="flex flex-col items-center overflow-y-hidden">
       <table className="relative mx-auto table-auto text-neutral-900 dark:text-neutral-100">
         <thead className="bg-neutral-200 dark:bg-neutral-700">
-        <tr>
-          {columns.map((col) => {
-            let colName = isString(col) ? col : Object.values(col)[0].alias || Object.keys(col)[0];
-            const colProp = isString(col) ? col : Object.keys(col)[0];
-            if (colName === 'actions') {
-              colName = actionsColumnName || colName;
-            }
+          <tr>
+            {columns.map((col) => {
+              let colName = isString(col) ? col : Object.values(col)[0].alias || Object.keys(col)[0];
+              const colProp = isString(col) ? col : Object.keys(col)[0];
+              if (colName === "actions") {
+                colName = actionsColumnName || colName;
+              }
 
-            return (
-              <th key={colName} className="p-3 sm:p-4">
-                <div className={`flex justify-center gap-2`}>
-                  <p className="font-bold">{capitalize(colName)}</p>
-                  {columnSortDirection[colProp] === 'asc' && (
-                    <ChevronDownIcon onClick={() => sort(colProp, 'desc')} className="h-6 w-6 cursor-pointer" />
-                  )}
-                  {columnSortDirection[colProp] === 'desc' && (
-                    <ChevronUpIcon onClick={() => sort(colProp, 'asc')} className="h-6 w-6 cursor-pointer" />
-                  )}
-                  {colName !== 'actions' && !columnSortDirection[colProp] && (
-                    <ChevronUpDownIcon onClick={() => sort(colProp, 'asc')} className="h-6 w-6 cursor-pointer" />
-                  )}
-                </div>
-              </th>
-            );
-          })}
-        </tr>
+              return (
+                <th key={colName} className="p-3 sm:p-4">
+                  <div className={`flex justify-center gap-2`}>
+                    <p className="font-bold">{capitalize(colName)}</p>
+                    {columnSortDirection[colProp] === "asc" && (
+                      <ChevronDownIcon onClick={() => sort(colProp, "desc")} className="h-6 w-6 cursor-pointer" />
+                    )}
+                    {columnSortDirection[colProp] === "desc" && (
+                      <ChevronUpIcon onClick={() => sort(colProp, "asc")} className="h-6 w-6 cursor-pointer" />
+                    )}
+                    {colName !== "actions" && !columnSortDirection[colProp] && (
+                      <ChevronUpDownIcon onClick={() => sort(colProp, "asc")} className="h-6 w-6 cursor-pointer" />
+                    )}
+                  </div>
+                </th>
+              );
+            })}
+          </tr>
         </thead>
         <tbody
           className="bg-neutral-50 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-neutral-400 dark:bg-neutral-800"
           ref={tableBody}
         >
-        {!objArrayState.length && (
-          <tr>
-            <td className="p-2 sm:p-4" colSpan={columns.length}>
-              <div className="flex justify-center gap-2">
-                {loading ? <Loader className="mx-auto my-24" /> : `No ${entityName}s found.`}
-                {newLink && !loading && <IconLink title={`New ${entityName}`} href={newLink} Icon={PlusIcon} />}
-              </div>
-            </td>
-          </tr>
-        )}
-        {objArrayState.map((obj) => (
-          <GenericTableDataRow
-            key={obj.id}
-            obj={obj}
-            columns={columns}
-            actions={actions}
-            onRowAction={(...params) => onAction(...params, entityName)}
-          />
-        ))}
+          {!objArrayState.length && (
+            <tr>
+              <td className="p-2 sm:p-4" colSpan={columns.length}>
+                <div className="flex justify-center gap-2">
+                  {loading ? <Loader className="mx-auto my-24" /> : `No ${entityName}s found.`}
+                  {newLink && !loading && <IconLink title={`New ${entityName}`} href={newLink} Icon={PlusIcon} />}
+                </div>
+              </td>
+            </tr>
+          )}
+          {objArrayState.map((obj) => (
+            <GenericTableDataRow
+              key={obj.id}
+              obj={obj}
+              columns={columns}
+              actions={actions}
+              onRowAction={(...params) => onAction(...params, entityName)}
+            />
+          ))}
         </tbody>
         {(showCount || newLink) && (
           <tfoot>
-          <tr>
-            {newLink && (
-              <td colSpan={!showCount ? columns.length : 1}>
-                <IconLink title={`New ${entityName}`} label="Add new" href={newLink} Icon={PlusIcon} />
-              </td>
-            )}
-            {showCount && (
-              <>
-                {columns.length > 2 && <td colSpan={columns.length - (newLink ? 2 : 1)} />}
-                <td className="text-end">
-                  {objArrayState.length} {capitalize(entityName) + sOrNoS(objArrayState.length)}
+            <tr>
+              {newLink && (
+                <td colSpan={!showCount ? columns.length : 1}>
+                  <IconLink title={`New ${entityName}`} label="Add new" href={newLink} Icon={PlusIcon} />
                 </td>
-              </>
-            )}
-          </tr>
+              )}
+              {showCount && (
+                <>
+                  {columns.length > 2 && <td colSpan={columns.length - (newLink ? 2 : 1)} />}
+                  <td className="text-end">
+                    {objArrayState.length} {capitalize(entityName) + sOrNoS(objArrayState.length)}
+                  </td>
+                </>
+              )}
+            </tr>
           </tfoot>
         )}
       </table>
     </div>
   );
-};
+}
 
-const colPropsToOmit = ['className', 'key', 'alias', 'capitalize'];
+const colPropsToOmit = ["className", "key", "alias", "capitalize"];
 
 function GenericTableDataRow({ obj, columns, actions, onRowAction }) {
   const objColumnMap = {};
 
   columns.forEach((col) => {
     const [[colName, colProps]] = isString(col) ? [[col]] : Object.entries(col);
-    if (colName === 'actions') {
+    if (colName === "actions") {
       objColumnMap[colName] = { colProps, value: actions }; // Value of actions column are the actions itself
     } else {
       objColumnMap[colName] = { colProps, value: deepGet(obj, colName) };
@@ -155,7 +155,7 @@ function GenericTableDataRow({ obj, columns, actions, onRowAction }) {
   const formatActions = (name, value) =>
     value.map((actionObj) => {
       const [[action, elementFunc]] = Object.entries(actionObj);
-      if (typeof elementFunc !== 'function') {
+      if (typeof elementFunc !== "function") {
         console.warn(`No element function provided for action ${actionObj}`);
         return null;
       }
@@ -166,7 +166,7 @@ function GenericTableDataRow({ obj, columns, actions, onRowAction }) {
     <tr className="relative after:absolute after:left-0 after:h-[2px] after:w-full after:bg-neutral-400">
       {Object.entries(objColumnMap).map(([colName, colData]) => {
         const { value, colProps } = colData;
-        return colName === 'actions' ? (
+        return colName === "actions" ? (
           <td key={colName} className="p-3 sm:p-4">
             <div className="flex justify-center gap-2">{formatActions(colName, value)}</div>
           </td>
@@ -178,20 +178,24 @@ function GenericTableDataRow({ obj, columns, actions, onRowAction }) {
       })}
     </tr>
   );
-};
+}
 
-
-function IconLink({ href = '', onClick, Icon, title, label, ...props }) {
-
-  const utilStyles = {}
+function IconLink({ href = "", onClick, Icon, title, label, ...props }) {
+  const utilStyles = {};
 
   return (
-    <a className={`${utilStyles.link}group flex`} href={href} onClick={onClick} title={title ? title : 'New'} {...props}>
+    <a
+      className={`${utilStyles.link}group flex`}
+      href={href}
+      onClick={onClick}
+      title={title ? title : "New"}
+      {...props}
+    >
       {label && <span>{label}</span>}
       <Icon className="h-6 w-6 transition-transform duration-300 active:scale-95 group-hover:scale-[120%] group-hover:duration-75" />
     </a>
   );
-};
+}
 
 function Loader({ className }) {
   return (
@@ -201,8 +205,10 @@ function Loader({ className }) {
       fill="none"
       viewBox="0 0 24 24"
     >
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2"/>
-      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"/>
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" />
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z" />
     </svg>
   );
 }
+
+export default GenericTable;
